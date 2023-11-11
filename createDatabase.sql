8130c66cf7fdeab10a01cd768d830c2c96d32704
Fernando Henrique <fernandogynbsb@gmail.com>
2023-11-10 23:37:44 -0300

    chore: adicionadas as configurações iniciais

---
+++ createDatabase.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE projeto_hacka_kenzie;
---
+++ src/data-source.ts
@@ -0,0 +1,30 @@
+import "reflect-metadata";
+import "dotenv/config";
+import { DataSource, DataSourceOptions } from "typeorm";
+import path from "path";
+
+const dataSourceConfig = (): DataSourceOptions => {
+    const entityPath: string = path.join(__dirname, "./entities/**.{ts,js}");
+    const migrationPath: string = path.join(
+        __dirname,
+        "./migrations/**.{ts,js}"
+    );
+    //demo-M4-T18[WSL:Ubuntu]/src/entities/**.{ts,js} */
+
+    const dbUrl: string | undefined = process.env.DATABASE_URL;
+
+    if (!dbUrl) {
+        throw new Error('Missing env: var "DATABASE_URL"');
+    }
+
+    return {
+        type: "postgres",
+        url: dbUrl,
+        logging: true,
+        entities: [entityPath],
+        migrations: [migrationPath],
+        synchronize: false
+    };
+};
+
+export const AppDataSource = new DataSource(dataSourceConfig());
