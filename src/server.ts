1d6dba89b4a6e8054fa472161b5563f7325b61ea
mazuquiebe <mazuquiebe75@gmail.com>
2023-11-14 15:10:28 -0300

    chore: configs & dependencies adicionados e/ou atualizados

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
-import 'dotenv/config'
-import { app } from "./app"
-import { AppDataSource } from './data-source'
+import 'dotenv/config';
+import { app } from './app';
+import AppDataSource from './data-source';
 
+AppDataSource.initialize()
+  .then((): void => {
+    console.log('Database Conected');
 
-AppDataSource.initialize().then(():void =>{
-    console.log('Database COnected')
-    
-    const PORT: number = Number(process.env.PORT) || 3000
+    const PORT: number = Number(process.env.PORT) || 3000;
 
-    app.listen(PORT, ():void => console.log(`app is running at port ${PORT}`))
-    
-}).catch((error:unknown)=> console.log(error))
+    app.listen(PORT, (): void => console.log(`app is running at port ${PORT}`));
+  })
+  .catch((error: unknown) => console.log(error));
