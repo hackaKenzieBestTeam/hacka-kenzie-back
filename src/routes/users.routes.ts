a8d6216bd823bad5f817f6e911d3035facda4fd9
mazuquiebe <mazuquiebe75@gmail.com>
2023-11-16 01:16:31 -0300

    feat: updateUser route

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,14 +1,34 @@
 import { Router } from 'express';
 import * as c from '../controllers/users.controllers';
 import * as md from '../middlewares';
+import {
+  createUserRequestSchema,
+  loginUserRequestSchema,
+  updateUserRequestSchema
+} from '../schemas/users.schemas';
+
 const userRouter = Router();
 
 userRouter.post(
   '/register',
+  md.validateRequestBodyMiddleware(createUserRequestSchema),
   md.verifyDuplicatedCpfEmailOrUsername,
   c.createUserController
 );
-userRouter.post('/auth', c.loginUserController);
+
+userRouter.post(
+  '/auth',
+  md.validateRequestBodyMiddleware(loginUserRequestSchema),
+  c.loginUserController
+);
+
 userRouter.get('/profile', md.authenticationMiddleware, c.getUserController);
 
+userRouter.patch(
+  '/update',
+  md.authenticationMiddleware,
+  md.validateRequestBodyMiddleware(updateUserRequestSchema),
+  md.verifyDuplicatedCpfEmailOrUsername,
+  c.updateUserController
+);
 export default userRouter;
